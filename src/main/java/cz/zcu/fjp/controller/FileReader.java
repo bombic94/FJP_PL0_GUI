4991aae788dd975fae5e8363dac1fc94e5bf87bc
bohmannd <bohmannd@gmail.com>
2017-10-24 14:36:17 +0200

    Loading txt files with pl0 code.

---
+++ src/main/java/cz/zcu/fjp/controller/FileReader.java
@@ -0,0 +1,50 @@
+package cz.zcu.fjp.controller;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import cz.zcu.fjp.model.Instruction;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class FileReader {
+
+	private static FileReader instance = null;
+	
+	protected FileReader() {
+		
+	}
+	
+	public static FileReader getInstance() {
+		if(instance == null) {
+	         instance = new FileReader();
+	      }
+	      return instance;
+	}
+
+	public ObservableList<Instruction> getTable(File file) {
+		Collection<Instruction> list = null;
+		try {
+			list = Files.readAllLines(new File(file.getAbsolutePath()).toPath())
+			        .stream()
+			        .map(line -> {
+			            String[] details = line.split("\\s+");
+			            Instruction i = new Instruction();
+			            i.setIndex(Integer.parseInt(details[0]));
+			            i.setInstruction(details[1]);
+			            i.setLevel(Integer.parseInt(details[2]));
+			            i.setOperand(Integer.parseInt(details[3]));
+			            return i;
+			        })
+			        .collect(Collectors.toList());
+			ObservableList<Instruction> tableList = FXCollections.observableArrayList(list);
+			return tableList;
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
--- src/main/java/cz/zcu/fjp/controller/MainController.java
+++ src/main/java/cz/zcu/fjp/controller/MainController.java
@@ -1,5 +1,6 @@
 package cz.zcu.fjp.controller;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,19 +16,23 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.SplitPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class MainController implements Initializable {
 
     @FXML
-    private SplitPane splitPane;
+    private AnchorPane anchorPane;
     
     @FXML
     private Label future1;
@@ -92,66 +97,51 @@ public class MainController implements Initializable {
     @FXML
     private Button btnLoad;
     
+    
+    private File file;
+	private FileChooser fileChooser;
     private PL0Debugger pl0 = PL0Debugger.getInstance();
+    private FileReader fr = FileReader.getInstance();
 
     ObservableList<Instruction> instructions;
     @Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
     	
-    	instructions = FXCollections.observableArrayList(
-    		new Instruction(0, "JMP", 0, 18, "info"),
-    		new Instruction(1, "JMP", 0, 11, "info"),
-    		new Instruction(2, "JMP", 0, 3, "info"),
-    		new Instruction(3, "INT", 0, 4, "info"),
-    		new Instruction(4, "LIT", 0, 11, "info"),
-    		new Instruction(5, "STO", 0, 3, "info"),
-    		new Instruction(6, "LIT", 0, 22, "info"),
-    		new Instruction(7, "STO", 1, 3, "info"),
-    		new Instruction(8, "LIT", 0, 33, "info"),
-    		new Instruction(9, "STO", 2, 3, "info"),
-    		new Instruction(10, "RET", 0, 0, "info"),
-    		new Instruction(11, "INT", 0, 4, "info"),
-    		new Instruction(12, "LIT", 0, 10, "info"),
-    		new Instruction(13, "STO", 1, 3, "info"),
-    		new Instruction(14, "LIT", 0, 20, "info"),
-    		new Instruction(15, "STO", 0, 3, "info"),
-    		new Instruction(16, "CAL", 0, 3, "info"),
-    		new Instruction(17, "RET", 0, 0, "info"),
-    		new Instruction(18, "INT", 0, 4, "info"),
-    		new Instruction(19, "LIT", 0, 100, "info"),
-    		new Instruction(20, "STO", 0, 3, "info"),
-    		new Instruction(21, "CAL", 0, 11, "info"),
-    		new Instruction(22, "RET", 0, 0, "info")
-    	);
-    	tableInstructions.setItems(instructions);
+    	fileChooser = new FileChooser();
+		fileChooser.setTitle("Choose file to encrypt");
+		fileChooser.getExtensionFilters().addAll(
+		         new ExtensionFilter("Text Files", "*.txt"),
+		         new ExtensionFilter("All Files", "*.*"));
+		
     	tableInstructions.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Instruction>() {
     		
     	    @Override
     	    public void changed(ObservableValue<? extends Instruction> observable, Instruction oldValue, Instruction actual) {
-//    	    	if (instructions.size() > actual.getIndex() + 1) {
-//    	    		Instruction future = instructions.get(actual.getIndex() + 1);
-//    	    		Stack futureStack = pl0.getFutureStack(future);
-//    	    		future1.setText(futureStack.getInstructionCount() + "");
-//    	    		future2.setText(futureStack.getBasis() + "");
-//    	    		future3.setText(futureStack.getTop() + "");
-//    	    	} else {
-//    	    		future1.setText("-");
-//    	    		future2.setText("-");
-//    	    		future3.setText("-");
-//    	    	}
-    	    	Stack actualStack = pl0.getActualStack(actual);
-    	    	Stack futureStack = pl0.getFutureStack(actual);
-    	    	
-    	    	actual1.setText(actualStack.getInstructionCount() + "");    
-    	    	actual2.setText(actualStack.getBasis() + "");
-    	    	actual3.setText(actualStack.getTop() + "");
-    	    	
-	    		future1.setText(futureStack.getInstructionCount() + "");
-	    		future2.setText(futureStack.getBasis() + "");
-	    		future3.setText(futureStack.getTop() + "");
+
+    	    	if (actual != null) {
+	    	    	Stack actualStack = pl0.getActualStack(actual);
+	    	    	Stack futureStack = pl0.getFutureStack(actual);
+	    	    	
+	    	    	actual1.setText(actualStack.getInstructionCount() + "");    
+	    	    	actual2.setText(actualStack.getBasis() + "");
+	    	    	actual3.setText(actualStack.getTop() + "");
+	    	    	
+		    		future1.setText(futureStack.getInstructionCount() + "");
+		    		future2.setText(futureStack.getBasis() + "");
+		    		future3.setText(futureStack.getTop() + "");
+    	    	} else {	    	    	
+	    	    	actual1.setText("-");    
+	    	    	actual2.setText("-");
+	    	    	actual3.setText("-");
+	    	    	
+		    		future1.setText("-");
+		    		future2.setText("-");
+		    		future3.setText("-");
+    	    	}
     	    }
     	});
     	
+    	//TODO implement treetableview
     	final ObservableList<StackItem> stackItems = FXCollections.observableArrayList(
     		new StackItem(0, 7),
     		new StackItem(1, 5),
@@ -168,7 +158,19 @@ public class MainController implements Initializable {
     }
     @FXML
     void loadFile(ActionEvent event) {
+    	file = fileChooser.showOpenDialog(anchorPane.getScene().getWindow());
+    	instructions = fr.getTable(file);
+    	if (instructions != null) {
+    		this.reset(event);
+    		tableInstructions.setItems(instructions);
+    	} else {
+    		Alert alert = new Alert(AlertType.ERROR);
+    		alert.setTitle("Error");
+    		alert.setHeaderText("Error has happened during loading file");
+    		alert.setContentText("Please check that file is in 'txt' format and contains correct information and only spaces between them.");
 
+    		alert.showAndWait();
+    	}
     }
 
     @FXML
